f12f3c700b8bcd31b7f31b67908977f6e983a752
Fabian W <Mephisto_III@web.de>
2012-01-10 14:40:10 +0100

    bondy chvatal

--- src/Algorithmen/BondyChvatal.java
+++ src/Algorithmen/BondyChvatal.java
@@ -2,6 +2,7 @@ package Algorithmen;
 
 import java.util.ArrayList;
 
+import static Algorithmen.Utility.*;
 import Interfaces.Edge;
 import Interfaces.Graph;
 import Interfaces.Vertex;
@@ -45,24 +46,70 @@ public class BondyChvatal {
 	}
 
 	private static <V extends Vertex, E extends Edge> ArrayList<V> doBondyChvatal(Graph<V, E> g, V from) {
-		ArrayList<V> res = new ArrayList<V>();
+
+		Graph<V, E> hull = makeGraphl(g.getVertexs(), g.getEdges());
+		ArrayList<E> newEdges = new ArrayList<E>();
+		newEdges.addAll(g.getEdges());
+		ArrayList<V> blub = g.getVertexs();
+		ArrayList<V> hamPath = new ArrayList<V>();
+		ArrayList<E> resEdges = new ArrayList<E>();
+
+		boolean changed = true;
+		while(changed){
+			changed = false;
+			hull = makeGraphl(blub, newEdges);
+			
+			for(int i = 0; i < (hull.getNumOfVertexs()-1); i++){
+				for(int k = i+1; k < hull.getNumOfVertexs(); k++){
+					if(g.getEdgeBetween(g.getV(i), hull.getV(k)).size() == 0 
+					&& ((hull.getNeighbors(hull.getV(i)).size() + hull.getNeighbors(hull.getV(k)).size()) >= hull.getNumOfVertexs())
+					&& !hull.getEdges().contains(ne(i,k))){
+						newEdges.add((E) ne(i, k));
+						zugriffe++;
+						changed = true;
+					}
+				}
+			}
+		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		return res;
+		changed = true;
+		resEdges.addAll(hull.getEdges());
+		hamPath.add(from);
+		while(changed){
+
+			changed = false;
+			hull = makeGraphl(blub, resEdges);
+			V actV = hamPath.get(hamPath.size()-1);
+			for(V v : hull.getNeighbors(actV)){
+				if(hull.getEdges().contains((E) ne(actV.getId(), v.getId()))&& !hamPath.contains(v)){
+					hamPath.add(v);
+					zugriffe++;
+					for(V x: hull.getNeighbors(v)){
+						if(hamPath.contains(x) && hamPath.get(hamPath.size()-2) != x ){
+							resEdges.remove(ne(v.getId(), x.getId()));
+							resEdges.remove(ne(x.getId(), v.getId()));
+							zugriffe++;
+						}
+					}
+					changed = true;
+					break;
+				}
+			}
+		}
+		if(newEdges.contains((E) ne(hamPath.get(0).getId(), hamPath.get(hamPath.size()-1).getId()))
+		|| newEdges.contains((E) ne(hamPath.get(hamPath.size()-1).getId(), hamPath.get(0).getId()))){
+			hamPath.add(hamPath.get(0));	
+		}
+		System.out.println("..................");
+		for(E a : resEdges){
+
+			System.out.println(a.getId()[0] + "" + a.getId()[1]);
+
+		}
+		System.out.println("..................");
+		System.out.println(hamPath);
+
+		return hamPath;
 
 	}
 }
--- src/Tests/GraphTest.java
+++ src/Tests/GraphTest.java
@@ -23,7 +23,9 @@ public class GraphTest {
 	Graph<Vertex,CapacityEdge> g7 = makeDigraph(vertexs(5),ce(0,1,3),ce(0,3,2),ce(1,2,2),ce(2,3,1),ce(3,4,2),ce(2,5,2),ce(3,1,2),ce(3,4,2),ce(4,5,4));
 												//vertex(5) hei�t 6 Ecken: 0...5
 	Graph<Vertex,CapacityEdge> g8 = makeDigraph(vertexs(6), ce(0,1,3),ce(0,3,3),ce(1,2,4),ce(2,0,3),ce(2,3,1),ce(2,4,2),ce(3,4,2),ce(3,5,6),ce(4,1,1),ce(4,6,1),ce(5,6,9));
-	
+	Graph<Vertex,NormalEdge> g9 = makeGraph(vertexs(3),ne(0,1),ne(1,2),ne(2,3),ne(3,0));
+
+	Graph<Vertex,NormalEdge> g10 = makeGraph(vertexs(4),ne(0,1),ne(1,2),ne(2,3),ne(3,0),ne(0,4),ne(1,4),ne(2,4),ne(3,4));
 	public static Graph<Vertex,RatedEdge> g = makeGraph(vertexs(8),re(2,1,20),re(2,3,25),re(1,4,30),re(2,4,15),re(4,3,40),re(4,6,24),re(6,5,5),re(5,7,19));
 	@Test
 	public void test() {	
@@ -37,9 +39,9 @@ public class GraphTest {
 //		System.out.println(shortestWayFloydWarshall(g2,1,6,1000));				
 //		System.out.println(shortestWayDijkstra(g5,0,8,1000));
 //		System.out.println(shortestWayFloydWarshall(g,0,7,1000));	
-		shortestWayFordFulkerson(g7,0,5,100000);
+//		shortestWayFordFulkerson(g7,0,5,100000);
 //		System.out.println(shortestGoldbergTarjan(g3,1,5,1));
-		shortestWayEdmundKarp(g7,0,5,100000);
+//		shortestWayEdmundKarp(g7,0,5,100000);
 //		shortestWayEdmundKarp(g8,0,6,1);
 //		shortestWayEdmundKarp(g3,0,7,1);
 //		shortestWayEdmundKarp(g6,0,7,1);
@@ -50,7 +52,8 @@ public class GraphTest {
 //				System.out.print(d2 + " ");
 //			}
 //		}
-
+		bondyChvatal(g9, 0, 0);
+		bondyChvatal(g10, 2, 0);
 	}
 
 }
